c737f46dbd3b659b0ad25697757f27d6bfb420b5
Hein Rutjes <hrutjes@gmail.com>
2020-01-25 12:23:15 +0100

    [chore] Better React imports & file-name extensions

--- src/SharedElementSceneContext.ts
+++ src/SharedElementSceneContext.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import SharedElementSceneData from './SharedElementSceneData';
 
 const SharedElementSceneContext = React.createContext<SharedElementSceneData | null>(
