02fc2d74c7585d14e5ac99c7f80ed82d06f60c3b
Vu PHAM <phamhoaivu911@gmail.com>
2020-04-14 17:24:10 +0700

    Hide bottom tab the correct way
    https://reactnavigation.org/docs/hiding-tabbar-in-screens/

--- example/src/tests/BottomTabs2.tsx
+++ example/src/tests/BottomTabs2.tsx
@@ -2,58 +2,35 @@ import { createAppContainer } from "react-navigation";
 import { createSharedElementStackNavigator } from "react-navigation-shared-element";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
-import { createScreen, MasterScreen, DetailScreen } from "../screens";
-
-const StackNavigator1 = createSharedElementStackNavigator(
-  {
-    Master: createScreen(MasterScreen, "BottomStack1"),
-    Detail: DetailScreen
-  },
-  undefined,
-  {
-    name: "BottomStack1",
-    debug: true
-  }
-);
-
-StackNavigator1.navigationOptions = ({ navigation }) => {
-  let tabBarVisible = true;
-  if (navigation.state.index > 0) {
-    tabBarVisible = false;
-  }
-
-  return {
-    tabBarVisible
-  };
-};
-
-const StackNavigator2 = createSharedElementStackNavigator(
-  {
-    Master: createScreen(MasterScreen, "BottomStack2"),
-    Detail: DetailScreen
-  },
-  undefined,
-  {
-    name: "BottomStack2",
-    debug: true
-  }
-);
+import { MasterScreen, DetailScreen } from "../screens";
 
 const TabNavigator = createBottomTabNavigator({
   Tab1: {
     // @ts-ignore
-    screen: StackNavigator1,
+    screen: MasterScreen,
     navigationOptions: {
       title: "Stack 1"
     }
   },
   Tab2: {
     // @ts-ignore
-    screen: StackNavigator2,
+    screen: MasterScreen,
     navigationOptions: {
       title: "Stack 2"
     }
   }
 });
 
-export default createAppContainer(TabNavigator);
+const MainStack = createSharedElementStackNavigator(
+  {
+    Master: TabNavigator,
+    Detail: DetailScreen
+  },
+  undefined,
+  {
+    name: "MainStack",
+    debug: true
+  }
+);
+
+export default createAppContainer(MainStack);
