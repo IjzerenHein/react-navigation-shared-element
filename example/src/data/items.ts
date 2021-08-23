b8fc102ff75c97de5f9883dce8b27c79a43cbbfb
Hein Rutjes <hrutjes@gmail.com>
2021-08-23 15:42:49 +0200

    [test] Add character descriptions to example app

--- example/src/data/items.ts
+++ example/src/data/items.ts
@@ -5,35 +5,49 @@ export const items: Item[] = [
     id: "homelander",
     title: "Homelander",
     image: require("../../assets/homelander.png"),
+    description:
+      "The Homelander (John) is a fictional supervillain in the comic book series The Boys, created by Garth Ennis and Darick Robertson. The leader of a group of hedonistic and reckless superheroes funded by Vought dubbed The Seven, and the archenemy of CIA black ops agent Billy Butcher, the Homelander is depicted as an arrogant narcissist thought by Butcher to be a sadistic psychopath who cares little about the well-being of those he professes to protect.",
   },
   {
     id: "stormfront",
     title: "Stormfront",
     image: require("../../assets/stormfront.jpg"),
+    description:
+      "A female interpretation of the character appears in the second season of the television series, portrayed by Aya Cash. This version's real name is Klara Risinger. Despite her youthful looks and command of social media, she was born in 1919, Frederick Vought's first successful test subject for Compound V, and supported Nazi Germany.",
   },
   {
     id: "atrain",
     title: "A-Train",
     image: require("../../assets/atrain.jpg"),
+    description:
+      "A-Train is a superhero with super-speed whose carelessness was responsible for the accidental death of Wee Hughie's girlfriend in the first issue. A-Train was formerly a member of the Teenage Kix, but was promoted to the Seven as a replacement to Mister Marathon. He is the most juvenile and crudest member of the Seven, being the one who most openly enjoys humiliating Starlight.",
   },
   {
     id: "blacknoir",
     title: "Black Noir",
     image: require("../../assets/blacknoir.png"),
+    description:
+      'Black Noir is a long-time member of the Seven, almost always shown in silhouette with his face obscured. His powers include super strength and supposed skills as a pilot. He is stronger than even the Homelander; Mother\'s Milk states he can "[...] bench a dozen Mack trucks". While initially an enigma, it is revealed at the climax of the series that Black Noir is actually a clone of the Homelander, developed by Vought-American as a contingency, in case the leader of the Seven became a liability.',
   },
   {
     id: "queenmaeve",
     title: "Queen Maeve",
     image: require("../../assets/queenmaeve.jpg"),
+    description:
+      "Queen Maeve is a long-time member of the Seven; her powers include super-strength, flight, and invulnerability. It is suggested that Queen Maeve was more passionate about the Seven's mission than the other superheroes at one point, but found her spirit broken by the team's disastrous handling of the 9/11 attacks. It is also suggested that the 9/11 debacle is the source of her alcoholism.",
   },
   {
     id: "thedeep",
     title: "The Deep",
     image: require("../../assets/thedeep.jpeg"),
+    description:
+      'A long-time member of the Seven, The Deep is marketed by Vought-American as the "King of the Seas" and claims he cannot remove his helmet due to an Atlantean curse. The Deep is actually a man in a diving suit. His powers include super-strength, flight, and durability. The Deep is the most mature, civilized member of the team and often bears the brunt of other characters\' contempt, disregard, and racism.',
   },
   {
     id: "starlight",
     title: "Starlight",
     image: require("../../assets/starlight.jpg"),
+    description:
+      "Starlight, aka Annie January, is the latest member of the Seven. Her known powers are flight and the ability to project blinding light, though she is also hinted to have super-hearing. Starlight was formerly a member of the Young Americans superhero organization, and a conservative Christian. Upon joining the Seven, she is shocked to discover the other members' true nature. On her first visit, the Homelander gives her the choice of providing him, A-Train, and Black Noir with oral sex, or leaving the group.",
   },
 ];
--- example/src/data/types.ts
+++ example/src/data/types.ts
@@ -2,4 +2,5 @@ export interface Item {
   id: string;
   title: string;
   image: any;
+  description?: string;
 }
