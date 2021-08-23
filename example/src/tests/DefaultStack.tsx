77d1b9f8088c404d24ed26f61405d3d737802125
Hein Rutjes <hrutjes@gmail.com>
2021-08-23 15:29:12 +0200

    [test] Cleanup tests in example app

--- example/src/tests/DefaultStack.tsx
+++ example/src/tests/DefaultStack.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
 import { createSharedElementStackNavigator } from "react-navigation-shared-element";
 
-import { MainScreen, DetailScreen, createScreen } from "../screens";
+import { MainScreen, DetailScreen } from "../screens";
 
 const name = "DefaultStack";
 
@@ -11,25 +11,11 @@ const Stack = createSharedElementStackNavigator({
   debug: true,
 });
 
-const Detail1Screen = createScreen(DetailScreen, undefined, undefined, {
-  onPress: ({ navigation, item }: any) => {
-    navigation.push("Detail2", {
-      item,
-    });
-  },
-});
-
-const Detail2Screen = createScreen(DetailScreen, undefined, undefined, {
-  resizeMode: "contain",
-});
-Detail2Screen.displayName = "Detail2Screen";
-
 export default () => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name={name} component={MainScreen} />
-      <Stack.Screen name="Detail" component={Detail1Screen} />
-      <Stack.Screen name="Detail2" component={Detail2Screen} />
+      <Stack.Screen name="Detail" component={DetailScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
--- example/src/tests/ModalSlideFromBottomIOS.tsx
+++ example/src/tests/ModalSlideFromBottomIOS.tsx
@@ -3,7 +3,7 @@ import { TransitionPresets } from "@react-navigation/stack";
 import * as React from "react";
 import { createSharedElementStackNavigator } from "react-navigation-shared-element";
 
-import { MainScreen, DetailScreen, createScreen } from "../screens";
+import { MainScreen, DetailScreen } from "../screens";
 
 const name = "ModalSlideFromBottomIOS";
 
@@ -12,29 +12,15 @@ const Stack = createSharedElementStackNavigator({
   debug: true,
 });
 
-const Detail1Screen = createScreen(DetailScreen, undefined, undefined, {
-  onPress: ({ navigation, item }: any) => {
-    navigation.push("Detail2", {
-      item,
-    });
-  },
-});
-
-const Detail2Screen = createScreen(DetailScreen, undefined, undefined, {
-  resizeMode: "contain",
-});
-Detail2Screen.displayName = "Detail2Screen";
-
 export default () => (
   <NavigationContainer>
     <Stack.Navigator
       screenOptions={{
-        ...TransitionPresets.ModalSlideFromBottomIOS,
+        ...TransitionPresets[name],
       }}
     >
       <Stack.Screen name={name} component={MainScreen} />
-      <Stack.Screen name="Detail" component={Detail1Screen} />
-      <Stack.Screen name="Detail2" component={Detail2Screen} />
+      <Stack.Screen name="Detail" component={DetailScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
